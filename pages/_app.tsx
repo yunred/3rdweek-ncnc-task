c59bbb9d1984f9b923de6a7408782bbd2af6ce85
jyb8509 <bestdung8509@gmail.com>
2022-02-14 19:26:06 +0900

    Add : Nav 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import NavBar from '../components/Nav/NavBar'
+import NavBar from '../Components/Nav/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
