6c5fbde19fba96af6d3cfd5fc0c2ffff0460f442
WongueShin <won7963kr@gmail.com>
2022-02-15 00:05:42 +0900

    Modify: APICompo SSR CSR 예제를 완성해서 머지합니다.

--- Const/Const.ts
+++ Const/Const.ts
@@ -2,4 +2,4 @@ export const CONCATEGORY_API: string = "https://api2.ncnc.app/con-category1s";
 export const CONITEM_API: string = "https://api2.ncnc.app/con-items/";
 export const SOON: string = "soon";
 export const FAQTYPE_API: string = "https://api2.ncnc.app/qa-types";
-export const FAQCONTENT_API: string = "https://api2.ncnc.app/qas?qaTypeId=$";
+export const FAQCONTENT_API: string = "https://api2.ncnc.app/qas?qaTypeId=";
