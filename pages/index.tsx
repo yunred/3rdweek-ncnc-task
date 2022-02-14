22c231c30971c74616c082ce4fdc019314aa679e
zerochae <zerochae@kakao.com>
2022-02-15 00:22:42 +0900

    Add: MainPage 레이아웃 set

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next'
-import APICompo from '/Components/APICompo/index.APICompo'
-import Head from 'next/head'
-import Image from 'next/image'
+import * as C from "/Const/Const";
+import * as H from "/Hooks/Hooks.ts";
 import styles from '../styles/Home.module.css'
+import CategoryContainer from '/Components/CategoryContainer/index.CategoryContainer';
+import ProductContainer from '/Components/ProductContainer/index.ProductContainer';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({menuCategory,productContent} :any) => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <CategoryContainer CategoryData={menuCategory.conCategory1s}/>
+        <ProductContainer props={productContent.conItems}/>
       </main>
 
       <footer className={styles.footer}>
@@ -15,6 +18,18 @@ const Home: NextPage = () => {
       </footer>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const menuCategory = await H.useFetch(C.CONCATEGORY_API);
+  const productContent = await H.useFetch(`${C.CONITEM_API}/${C.SOON}`);
+
+  return {
+    props : {
+      menuCategory : menuCategory,
+      productContent: productContent
+    }
+  }
 }
 
 export default Home
