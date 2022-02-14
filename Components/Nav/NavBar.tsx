07bbe6a04e54d876a1ffa4ceb780c42a3e7591ee
jyb8509 <bestdung8509@gmail.com>
2022-02-14 19:34:55 +0900

    Add :Nav 추가

--- Components/Nav/NavBar.tsx
+++ Components/Nav/NavBar.tsx
@@ -3,6 +3,14 @@ import css from "styled-jsx/css";
 import Link from "next/link";
 import Image from 'next/image'
 
+const data = {
+    id: 67,
+    name: "카페",
+    discountRate: 50,
+    imageUrl: "https://d1dsr05o5i286u.cloudfront.net/a2402a35-038b-4b68-a152-2cc14dff532c.jpg"
+  };
+  
+
 interface NavDataType{
   src: string;
   alt: string;
@@ -16,17 +24,17 @@ const NavBar = (NavData:NavDataType): JSX.Element => {
     <nav>
       <Link href="/" passHref> 
         <div className="NavContainer">
-          <a>
-        <button>
-        <Image src="/images/hamburgermenu.png" alt="seemore" width="100%" height="100%" />
-        </button>
-        <button>
-        <Image src="/imgaes/leftarrow.png" alt="seeback" width="100%" height="100%"  />
-        </button>
-        </a>
-        <div>
-            니콘내콘  
-        </div>
+            <a>
+                <button>
+                <Image src="/images/hamburgermenu.png" alt="seemore" width="50%" height="50%" />
+                </button>
+                <button>
+                <Image src="/imgaes/leftarrow.png" alt="seeback" width="50%" height="50%"  />
+                </button>
+            </a>
+            <div>
+                <p>{data.name}</p>
+            </div>
         </div>
         
       </Link>
