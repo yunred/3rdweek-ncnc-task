8031d57a0a3722105c587a3f61dd5f1dd6e9c4ec
WongueShin <won7963kr@gmail.com>
2022-02-15 13:03:29 +0900

    ADD: /categories 세부 카테고리 페이지 초기버전

--- Const/Const.ts
+++ Const/Const.ts
@@ -1,4 +1,5 @@
-export const CONCATEGORY_API: string = "https://api2.ncnc.app/con-category1s";
+export const CONCATEGORY_API: string = "https://api2.ncnc.app/con-category1s/";
+export const NESTED : string = "/nested";
 export const CONITEM_API: string = "https://api2.ncnc.app/con-items/";
 export const SOON: string = "soon";
 export const FAQTYPE_API: string = "https://api2.ncnc.app/qa-types";
