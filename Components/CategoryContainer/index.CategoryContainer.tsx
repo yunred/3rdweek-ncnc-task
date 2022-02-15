8f55d39b4493d4aac60b858c04cf5313fb843650
zerochae <zerochae@kakao.com>
2022-02-15 12:49:07 +0900

    Fix: Category Container Type 오류 수정

--- Components/CategoryContainer/index.CategoryContainer.tsx
+++ Components/CategoryContainer/index.CategoryContainer.tsx
@@ -2,17 +2,16 @@ import style from "Components/CategoryContainer/CategoryContainer.module.css";
 import Category from "Components/Category/index.Category";
 
 interface CategoryProps {
-  map(arg0: (item: CategoryProps, index: number) => JSX.Element): import("react").ReactNode;
   id: number;
   name: string;
   discountRate: number;
   imageUrl: string;
 }
 
-const CategoryContainer = ({CategoryData} :{CategoryData:CategoryProps}) => {
+const CategoryContainer = ({CategoryData} :{CategoryData:CategoryProps[]}) => {
   return (
     <div className={style.Container}>
-      {CategoryData.map((item: CategoryProps,index :number) => {
+      {CategoryData.map((item: CategoryProps,index :number) =>  {
         return (
           <div key={index} className={style.innerContainer}>
             <Category CategoryData={item} />
