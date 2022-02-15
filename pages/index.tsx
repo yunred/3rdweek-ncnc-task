f5cfb8291b7b4a4cdff58194e42a856e9589c8ff
WongueShin <won7963kr@gmail.com>
2022-02-15 19:09:58 +0900

    Modify: pages/ serversideProps에 현제 페이지 정보를 의미하는 props를 추가했습니다;

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ interface HomeProps {
   productContent: ProductProps[]
 }
 
-const Home:NextPage<HomeProps> = ({ menuCategory, productContent }) => {
+const Home:NextPage<HomeProps> = ({ currentPage, menuCategory, productContent }) => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -38,6 +38,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: {
+      currentPage: 'home',
       menuCategory: menuCategory.conCategory1s,
       productContent: productContent.conItems,
     },
