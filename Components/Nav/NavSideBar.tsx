6a9839c57567ff86866498e826573cfb2d9f3937
jyb8509 <bestdung8509@gmail.com>
2022-02-15 20:16:21 +0900

    Modify: merge를 위한 commit

--- Components/Nav/NavSideBar.tsx
+++ Components/Nav/NavSideBar.tsx
@@ -1,29 +1,33 @@
+import { useRouter } from 'next/router'
+import { useState, useEffect } from "react";
 import style from "./NavBar.module.css";
 import css from "styled-jsx/css";
 import Link from "next/link";
 import Image from 'next/image'
 
 const NavSideBar = (): JSX.Element => {
+    const goBackHandler = () =>{
+        window.location.reload(false);
+    }
 
     return (
-        <div className={style.sideBarwrapperContainer}>
-                <div className={style.myPageWrap}>
+
+        <div className={style.sideBarwrapperContainer} onClick={goBackHandler}  >
+                <div className={style.myPageWrap} >
                     <p>마이페이지</p>       
                 </div>
                 <div className={style.sideBarline} /> 
 
-                <Link href="/" passHref> 
+                <Link href={`/brands/63`} >
                         <a className={style.sideBarIcon} > 
                             <div> 고객센터 </div> 
                             <div> > </div> 
                         </a> 
                 </Link>
-                <div className={style.sideBarbottom} /> 
-                <div className={style.sideBarLastLine}/> 
+                <div className={style.sideBarbottom} />        
+                <div className={style.sideBarLastLine} onClick={goBackHandler} /> 
         </div>
-)
-
-
+    )
 };
 
 export default NavSideBar;
