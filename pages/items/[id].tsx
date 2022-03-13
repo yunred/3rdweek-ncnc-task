29cf6d54692a575877bcdc5213debf4f63ec7347
SEOYUN KIM <nado0903@gmail.com>
2022-03-13 23:32:04 +0900

    Fix: pages/items/[id].tsx useEffect async 코드 수정

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -1,12 +1,12 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { useEffect, useState, useCallback } from 'react';
 
-import * as H from "Hooks/Hooks";
-import * as C from "Const/Const";
-import * as T from "Types/Types";
+import * as H from 'Hooks/Hooks';
+import * as C from 'Const/Const';
+import * as T from 'Types/Types';
 
-import styles from "styles/Item.module.css";
-import ItemContainer from "Components/ItemContainer/index.ItemContainer";
+import styles from 'styles/Item.module.css';
+import ItemContainer from 'Components/ItemContainer/index.ItemContainer';
 
 import NavBar from 'Components/Nav/NavBar';
 
@@ -14,25 +14,25 @@ interface ItemPageProps {
   currentId: number;
 }
 
-const Items = ({ currentId }:ItemPageProps) => {
+const Items = ({ currentId }: ItemPageProps) => {
   const [itemData, setItemData] = useState<T.ItemProps>();
-
+  const FetchItem = useCallback(async () => {
+    const res = await H.Fetch(`${C.CONITEM_API}/${currentId}`);
+    setItemData(res.conItem);
+  }, [currentId]);
   useEffect(() => {
-    (async () => {
-      const res = await H.Fetch(`${C.CONITEM_API}/${currentId}`);
-      setItemData(res.conItem);
-    })();
-  }, []);
+    FetchItem();
+  }, [FetchItem]);
 
   return (
     <>
-    <NavBar/>
-    <div className={styles.container}>
-      <main className={styles.main}>
+      <NavBar />
+      <div className={styles.container}>
+        <main className={styles.main}>
           {itemData && <ItemContainer itemData={itemData} />}
-      </main>
-      <footer className={styles.footer}></footer>
-    </div>
+        </main>
+        <footer className={styles.footer}></footer>
+      </div>
     </>
   );
 };
