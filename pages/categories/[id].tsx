87c28ddcd8d9b38508b7de774bad9a04b161ccdb
SEOYUN KIM <nado0903@gmail.com>
2022-03-14 16:16:47 +0900

    Modify: pages/categories/[id].tsx 렌더링 방식 변경

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -1,54 +1,85 @@
-import type { NextPage, GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { useRouter } from 'next/router'
-import styles from 'styles/Categories.module.css'
-import * as C from "Const/Const";
-import * as H from "Hooks/Hooks";
-import { useState, useEffect } from 'react';
+import type {
+  NextPage,
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPaths,
+} from 'next';
+import { useRouter } from 'next/router';
+import styles from 'styles/Categories.module.css';
+import * as T from 'Types/Types';
+import * as C from 'Const/Const';
+import * as H from 'Hooks/Hooks';
+import { useState, useEffect, useCallback } from 'react';
 import CategoryContainer from 'Components/CategoryContainer/index.CategoryContainer';
 import ProductContainer from 'Components/ProductContainer/index.ProductContainer';
 import NavBar from 'Components/Nav/NavBar';
+import { ProductProps } from 'Components/Types/ProductType';
 
 interface CategoriesProps {
-  currentPage: string,
-  curruntID: string,
-  categoryProps: any,
-  category1Props: any[]
+  currentPage: string;
+  curruntID: string;
+  categoryProps: any;
 }
 
-const Categories: NextPage<CategoriesProps> = ({currentPage, curruntID, categoryProps, category1Props}) => {
-    const router = useRouter();
-    return (
-      <>
-      <NavBar/>
+const Categories: NextPage<CategoriesProps> = ({
+  currentPage,
+  curruntID,
+  categoryProps,
+}) => {
+  const [category1Props, setCategory1Props] = useState<
+    ProductProps[] | undefined
+  >();
+  const FetchProductContent = useCallback(async () => {
+    const res = await H.Fetch(`${C.CONITEM_API}/${C.SOON}`);
+    setCategory1Props(
+      res.conItems.sort((a: { id: number }, b: { id: number }) => a.id - b.id)
+    );
+  }, []);
+  useEffect(() => {
+    FetchProductContent();
+  }, [FetchProductContent]);
+  return (
+    <>
+      <NavBar />
       <div className={styles.container}>
         <main className={styles.main}>
-          {curruntID !== "1"?
-          <CategoryContainer CategoryData={categoryProps} currentPage={currentPage}/>
-          :
-          <ProductContainer ProductData = {category1Props}/>
-        }
+          {curruntID !== '1' ? (
+            <CategoryContainer
+              CategoryData={categoryProps}
+              currentPage={currentPage}
+            />
+          ) : (
+            <ProductContainer ProductData={category1Props} />
+          )}
         </main>
-        <footer className={styles.footer}>
-          
-        </footer>
+        <footer className={styles.footer}></footer>
       </div>
-      </>
-    )
-  }
-  
-export const getServerSideProps: GetServerSideProps = async (context:GetServerSidePropsContext) => {
-  const curruntID = context.params? context.params.id: undefined;
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pathList = ['1', '67', '62', '60', '61', '65', '129', '69', '128'];
+  const paths = pathList.map((categoryItem: any) => ({
+    params: { id: categoryItem.toString() },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const curruntID = context.params ? context.params.id : undefined;
   const categoryProps = await H.Fetch(C.CONCATEGORY_API + curruntID + C.NESTED);
-  const category1Props = await H.Fetch(C.CONITEM_API + C.SOON);
   return {
     props: {
       currentPage: 'categories',
       curruntID: curruntID,
       categoryProps: categoryProps.conCategory1.conCategory2s,
-      category1Props: category1Props.conItems.sort((a: { id: number; }, b: { id: number; })=> a.id - b.id)
     },
   };
 };
 
-
-  export default Categories
+export default Categories;
