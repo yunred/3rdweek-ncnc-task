891120733c32fd7b6b7cd190fd4d808f4acc5138
SEOYUN KIM <nado0903@gmail.com>
2022-03-14 19:06:06 +0900

    Modify: pages/item/[id].tsx 렌더링 방식 변경

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -1,52 +1,42 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from 'react';
 
-import * as H from "Hooks/Hooks";
-import * as C from "Const/Const";
-import * as T from "Types/Types";
+import * as H from 'Hooks/Hooks';
+import * as C from 'Const/Const';
+import * as T from 'Types/Types';
 
-import styles from "styles/Item.module.css";
-import ItemContainer from "Components/ItemContainer/index.ItemContainer";
-
+import styles from 'styles/Item.module.css';
+import ItemContainer from 'Components/ItemContainer/index.ItemContainer';
+import { useRouter } from 'next/router';
 import NavBar from 'Components/Nav/NavBar';
 
-interface ItemPageProps {
-  currentId: number;
-}
-
-const Items = ({ currentId }:ItemPageProps) => {
+const Items = () => {
   const [itemData, setItemData] = useState<T.ItemProps>();
-
+  const router = useRouter();
+  const [currentId, setCurrentId] = useState<number>();
+  const FetchItemData = useCallback(async () => {
+    const res = await H.Fetch(`${C.CONITEM_API}/${currentId}`);
+    setItemData(res.conItem);
+  }, [currentId]);
   useEffect(() => {
-    (async () => {
-      const res = await H.Fetch(`${C.CONITEM_API}/${currentId}`);
-      setItemData(res.conItem);
-    })();
-  }, []);
+    if (router.query.id && typeof router.query.id !== 'object') {
+      setCurrentId(parseInt(router.query.id));
+      if (currentId !== undefined) {
+        FetchItemData();
+      }
+    }
+  }, [FetchItemData, currentId, router.query.id]);
 
   return (
     <>
-    <NavBar/>
-    <div className={styles.container}>
-      <main className={styles.main}>
+      <NavBar />
+      <div className={styles.container}>
+        <main className={styles.main}>
           {itemData && <ItemContainer itemData={itemData} />}
-      </main>
-      <footer className={styles.footer}></footer>
-    </div>
+        </main>
+        <footer className={styles.footer}></footer>
+      </div>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const currentId = context.params ? context.params.id : undefined;
-
-  return {
-    props: {
-      currentId: currentId,
-    },
-  };
 };
 
 export default Items;
